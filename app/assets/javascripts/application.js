5f31ec4f2b65c3a3811fa4a2fe65b2ea9a5ad74c
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2016-03-05 22:22:11 -0600

    Reorganized JS scripts
    Cut js scripts from application.html.erb and put them in application.js
    just to clean up. Also changed logo resize to 50 from 100 to
    synchronize nav scroll animation.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,21 @@
 //= require jquery
 
-// http://learnwebtutorials.com/jquery-tutorial-animate-scroll-to-anchor-link
+// Nav scroll animations - http://learnwebtutorials.com/jquery-tutorial-animate-scroll-to-anchor-link
+//// About section
 $(document).ready(function() {
   $('a[href="#about"]').click(function(e) {
     e.preventDefault();
     var targetOffset = $('div[name="about"]').offset().top;
     $('body').animate({scrollTop: targetOffset}, 1000);
   });
+});
+////
+
+// Nav logo resize animation
+$(document).on("scroll",function(){
+  if($(document).scrollTop()>50){
+    $("img").removeClass("large").addClass("small");
+  } else{
+    $("img").removeClass("small").addClass("large");
+  }
 });
