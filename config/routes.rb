c515e9ad689dd35297a2cc820cc2f5024673bdc1
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2016-03-09 03:12:49 -0600

    Fixed route and moved footer
    Changed redirect after login to user profile, fixed sign_out route and
    moved footer.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'members/profile'
 
-  get 'auth/:provider/callback', to: "sessions#create"
+  get 'members/sign_out', to: "sessions#destroy", as: 'sign_out'
 
-  get'sign_out', to: "sessions#destroy", as: 'sign_out'
+  get 'auth/:provider/callback', to: "sessions#create"
 
   root to: 'welcome#index'
 end
